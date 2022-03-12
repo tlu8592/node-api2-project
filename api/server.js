fde0ab75f6179f6b0335b0f6232c5c90cc74b3e0
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-03-11 23:51:58 -0500

    server setup

--- api/server.js
+++ api/server.js
@@ -1,2 +1,11 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/', (req, res) => res.send('API running'));
+
+module.exports = server;
